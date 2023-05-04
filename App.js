8a5dbc7a52b2f70b550fb5bf5469985288b227ff
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2023-05-04 17:12:19 -0400

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import * as Font from "expo-font";
 
 import LoginScreen from "./screens/Login";
 import BottomTabNavigator from "./components/BottomTabNavigator";
-
+ 
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
 export default class App extends Component {
