61d34ef2d484090082d6677e14f7d82debf95081
Aayush-Tadhani246 <70973409+Aayush-Tadhani246@users.noreply.github.com>
2023-05-04 17:11:49 -0400

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from "react"; 
 import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
